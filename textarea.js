8de9dd7def24c1f9b855e66539b0589574420d1c
Adam Michael <ajmichael0@gmail.com>
2014-07-04 21:07:38 -0400

    Added block cursor

--- textarea.js
+++ textarea.js
@@ -1,23 +1,34 @@
-function SetCursorPosition(pos) {
-    var obj = document.getElementById('mainta'),
-    range.collapse(true);
-    range.moveEnd('character', pos);
-    range.moveStart('character', pos);
-    range.select();
+var cursor;
+
+function $(elid) {
+    return document.getElementById(elid);
 }
 
+function nl2br(txt) {
+    return txt.replace(/\n/g, "<br />");
+}
+
+function writeIt(from, e) {
+    e = e || window.event;
+    var w = $("writer");
+    var tav = from.value;
+    w.innerHTML = nl2br(tav);
+}
+
+function moveIt(count, e) {
+    e = e || window.event;
+    var keycode = e.keyCode || e.which;
+    if (keycode == 37 && parseInt(cursor.style.left) >= (0-((count-1)*10))) {
+        // left arrow key
+        cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
+    } else if (keycode == 39 && (parseInt(cursor.style.left) + 10) <= 0) {
+        // right arrow key
+        cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
+    }
+}
 
 window.onload = function() {
-    var ta = document.getElementById('mainta');
-
-    ta.onkeypress = function(e) {
-        var keycode = e.which || e.keyCode,
-            key;
-        key = String.fromCharCode(keycode);
-        if (keycode == 48) {    // '0'
-            e.preventDefault();
-            SetCursorPosition(0);
-        }
-    }
+    cursor = $("cursor");
+    cursor.style.left = "0px";
 
 };
